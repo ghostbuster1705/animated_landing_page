805988fe020966535b0f9997617691912582b055
ghostbuster1705 <alexander17058917@gmail.com>
2025-01-19 14:18:41 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,95 @@
+export const mapClasses = [
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+];
+
+export const previews = [
+
+    {
+        img: "./assets/main-1.jpg",
+        title: "Ra",
+        tags: "Futuristic Fashion, Minimal Design",
+        description:
+            "Exploring the intersection of minimalism and future fashion trends in web design.",
+    },
+
+    {
+        img: "./assets/main-2.jpg",
+        title: "Uptask",
+        tags: "Fashion Innovation, Graphic Simplicity",
+        description:
+            "Innovative fashion-forward web design with a core focus on simplicity and elegance.",
+    },
+
+    {
+        img: "./assets/main-3.jpg",
+        title: "Genix",
+        tags: "Eco Fashion, Substainable Design",
+        description:
+            "Substainable fashion meets modern web aesthetics, highlighting eco-friendly apparel innovations.",
+    },
+
+    {
+        img: "./assets/main-4.jpg",
+        title: "Unor",
+        tags: "Digital Fashion, UI/UX",
+        description:
+            "Digital-first fashion branding that merges cutting-edge UI/UX principles with style.",
+    },
+
+    {
+        img: "./assets/main-5.jpg",
+        title: "Xav",
+        tags: "Interactive Design, Fashion Tech",
+        description:
+            "Digital-first fashion branding that merges cutting-edge UI/UX principles with style.",
+    },
+
+    {
+        img: "./assets/main-6.jpg",
+        title: "Maxim Stark",
+        tags: "Futuristic Web Graphics",
+        description:
+            "Digital-first fashion branding that merges cutting-edge UI/UX principles with style.",
+    },
+
+    {
+        img: "./assets/main-7.jpg",
+        title: "Pitcher",
+        tags: "Innovative textiles",
+        description:
+            "Digital-first fashion branding that merges cutting-edge UI/UX principles with style.",
+    },
+
+    {
+        img: "./assets/main-8.jpg",
+        title: "MindSpace",
+        tags: "Innovative textiles",
+        description:
+            "Digital-first fashion branding that merges cutting-edge UI/UX principles with style.",
+    },
+
+    {
+        img: "./assets/main-9.jpg",
+        title: "The Athletics",
+        tags: "Innovative textiles",
+        description:
+            "Digital-first fashion branding that merges cutting-edge UI/UX principles with style.",
+    },
+
+    {
+        img: "./assets/main-10.jpg",
+        title: "All work",
+        tags: "Innovative textiles",
+        description:
+            "Digital-first fashion branding that merges cutting-edge UI/UX principles with style.",
+    },
+];
